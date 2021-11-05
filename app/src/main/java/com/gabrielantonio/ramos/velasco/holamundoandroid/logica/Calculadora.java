12e6a34b1ac156a702ed51fb771cbcbab5d9398b
GabrielAntonioRamosVelasco <ramos.velasco.gabriel.antonio@gmail.com>
2021-11-05 09:42:01 -0600

    Tercera Version

---
+++ app/src/main/java/com/gabrielantonio/ramos/velasco/holamundoandroid/logica/Calculadora.java
@@ -0,0 +1,25 @@
+package com.gabrielantonio.ramos.velasco.holamundoandroid.logica;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable{
+
+    double numero1,numero2;
+
+    public void suma(){
+
+    }
+
+    public void resta(){
+
+    }
+
+    public void multiplicacion(){
+
+    }
+
+    public void division(){
+
+    }
+
+}
